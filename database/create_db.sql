57e3ccb51412f01e98f683542ff708eeefc8c97a
fyari <fyari@users.noreply.github.com>
2020-01-24 17:16:48 +0100

    Update create_db.sql

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,46 +1,31 @@
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS job;
+
+DROP TABLE IF EXISTS task;
 DROP TABLE IF EXISTS input;
 DROP TABLE IF EXISTS result;
-DROP TABLE IF EXISTS jobmanager;
 
-create table user (
-    id 					      integer primary key  autoincrement,
-    username 		 varchar(50) UNIQUE NOT NULL,
-    password 		 varchar(50) NOT NULL
-);
-
-create table job (
-     id                  integer primary key  autoincrement,
-    user_id 		integer not null,
-    jobname 	     varchar(50) unique not null, 
+create table task (
+     id                 integer primary key  autoincrement,
+    taskname 	 varchar(50) unique not null, 
     created	    timestamp not null DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES user (id)
+    status           varchar(50) not null,  // result_ready , result_not_ready
+  
 );
 
 create table input (
-    jobid 			  integer primary key  autoincrement,
+    id                  integer primary key  autoincrement,
+    task_id 			  integer primary key  autoincrement,
     param1          double precision ,
     param2          double precision,      	
     param2          double precision,      
+    FOREIGN KEY (job_id) REFERENCES user (task)
   );
 create table result (
-     job_id 					integer primary key  ,
+     id                        integer primary key  autoincrement,
+     task_id 				integer primary key  ,
     result1		            double precision ,
     result1		            double precision ,
     result1		            double precision ,
-    -- x , y , z , ...
-    FOREIGN KEY (job_id) REFERENCES user (job)
+  
+    FOREIGN KEY (job_id) REFERENCES user (task)
   
   );
-
-/*
-CREATE TABLE jobmanager (
-  id 				INTEGER PRIMARY KEY AUTOINCREMENT,
-  --user_id 		INTEGER NOT NULL,
-  job_id 	    TEXT UNIQUE NOT NULL, 
-  status			TEXT NOT NULL,
-  starttime	    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  endtime		TIMESTAMP NULL 
-);
-*/
