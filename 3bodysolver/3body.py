3c61d0eddca49e96969b27b7caf89eba5e0a94d6
fyari <fyari@users.noreply.github.com>
2020-01-24 23:13:28 +0100

    Update 3body.py

--- 3bodysolver/3body.py
+++ 3bodysolver/3body.py
@@ -1,45 +1,35 @@
-#Mass of the Third Star
-m3=1.0 #Third Star#Position of the Third Star
-r3=[0,1,0] #m
-r3=sci.array(r3,dtype="float64")#Velocity of the Third Star
-v3=[0,-0.01,0]
-v3=sci.array(v3,dtype="float64")
+import json
+import requests
+import base64
+import time
 
-#Update COM formula
-r_com=(m1*r1+m2*r2+m3*r3)/(m1+m2+m3)#Update velocity of COM formula
-v_com=(m1*v1+m2*v2+m3*v3)/(m1+m2+m3)
+rk_id = 1 
 
+url             = "192.168.17.4/api/v1/"
+get_task_url    = url + "gettask"
+save_result_url = url + "saveresult"
 
-def ThreeBodyEquations(w,t,G,m1,m2,m3):
-    r1=w[:3]
-    r2=w[3:6]
-    r3=w[6:9]
-    v1=w[9:12]
-    v2=w[12:15]
-    v3=w[15:18]    r12=sci.linalg.norm(r2-r1)
-    r13=sci.linalg.norm(r3-r1)
-    r23=sci.linalg.norm(r3-r2)
-    
-    dv1bydt=K1*m2*(r2-r1)/r12**3+K1*m3*(r3-r1)/r13**3
-    dv2bydt=K1*m1*(r1-r2)/r12**3+K1*m3*(r3-r2)/r23**3
-    dv3bydt=K1*m1*(r1-r3)/r13**3+K1*m2*(r2-r3)/r23**3
-    dr1bydt=K2*v1
-    dr2bydt=K2*v2
-    dr3bydt=K2*v3    r12_derivs=sci.concatenate((dr1bydt,dr2bydt))
-    r_derivs=sci.concatenate((r12_derivs,dr3bydt))
-    v12_derivs=sci.concatenate((dv1bydt,dv2bydt))
-    v_derivs=sci.concatenate((v12_derivs,dv3bydt))
-    derivs=sci.concatenate((r_derivs,v_derivs))
-    return derivs
-    
-    
-#Package initial parameters
-init_params=sci.array([r1,r2,r3,v1,v2,v3]) #Initial parameters
-init_params=init_params.flatten() #Flatten to make 1D array
-time_span=sci.linspace(0,20,500) #20 orbital periods and 500 points#Run the ODE solver
-import scipy.integratethree_body_sol=sci.integrate.odeint(ThreeBodyEquations,init_params,time_span,args=(G,m1,m2,m3))
+params = []
+
+def getTasks(rk_id):
+    response = requests.get(get_task_url + str(rk_id), verify=False)
+    if(response.ok):
+        a=[]
+def saveResult(res):
+    response = requests.get(save_result_url + str(res), verify=False)
+
+def rksolver():
+    pass
 
 
-r1_sol=three_body_sol[:,:3]
-r2_sol=three_body_sol[:,3:6]
-r3_sol=three_body_sol[:,6:9]
+if __name__ == "__main__":
+
+    while(True):
+        task = getTasks()
+        # params 
+        params = []
+        res = rksolver()
+
+        saveResult(res)
+
+        time.sleep(1)
