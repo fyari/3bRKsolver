1888988ea871925637001abb76fff0fc1d91e4bd
fyari <fyari@users.noreply.github.com>
2020-01-25 21:58:40 +0100

    Update 3body.py

--- 3bodysolver/3body.py
+++ 3bodysolver/3body.py
@@ -12,17 +12,37 @@ save_result_url = url + "save_result"
 
 params = []
 
+class Task:
+    def __init__(self, id , taskname, mx , my , mz):
+        self.id = id
+        self.name = taskname
+        self.mx = mx
+        self.my = my
+        self.mz = mz
+     def __init__(self):
+        self.id = 0
+        self.name = "taskname"
+        self.mx = 1.0
+        self.my = 1.0
+        self.mz = 1.0
+    def print(self):
+        print self.id + " " + self.name
+
 def getTasks(rk_id):
+    task = Task()
+    # task = [id , taskname , mx , my , mz]
     response = requests.get(get_task_url + str(rk_id), verify=False)
     if(response.ok):
-        a=[]
+        pass
+    return task
+
 def saveResult(res):
     response = requests.get(save_result_url + str(res), verify=False)
 
-def rksolver():
+def rksolver(mx = 2e24, my = 5e30, mz =1e27):
     #get the inputs here from master
-    results = solver.threeBody(2e24,5e30,1e27)
-    #save results, results is a list of 4 elements
+    results = solver.threeBody(mx,my,mz)
+    return results
 
 
 if __name__ == "__main__":
