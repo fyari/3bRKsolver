d97c226c8652541d37e2d3d38259570acdaeb1cb
prak-huded <55449760+prak-huded@users.noreply.github.com>
2020-01-25 17:40:22 +0100

    Add files via upload

--- 3bodysolver/3body.py
+++ 3bodysolver/3body.py
@@ -2,12 +2,13 @@ import json
 import requests
 import base64
 import time
+import body_prob as solver
 
 rk_id = 1 
 
-url             = "192.168.17.4/api/v1/"
-get_task_url    = url + "gettask"
-save_result_url = url + "saveresult"
+url             = "192.168.17.4:5000/master/"
+get_task_url    = url + "get_tasks"
+save_result_url = url + "save_result"
 
 params = []
 
@@ -19,7 +20,9 @@ def saveResult(res):
     response = requests.get(save_result_url + str(res), verify=False)
 
 def rksolver():
-    pass
+    #get the inputs here from master
+    results = solver.threeBody(2e24,5e30,1e27)
+    #save results, results is a list of 4 elements
 
 
 if __name__ == "__main__":
