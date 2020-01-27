02e359a6ba62297d8b71ae0649ca9f32abb840d1
sidd332 <59736075+sidd332@users.noreply.github.com>
2020-01-27 16:39:41 +0100

    Add files via upload

--- webapp/app.py
+++ webapp/app.py
@@ -1,59 +1,86 @@
 from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
-import datetime
-
+from datetime import datetime
 
 app = Flask(__name__)
 
-POSTGRES = {
-    'user': 'postgres',
-    'pw': '1132',
-    'db': 'threebody',
-    'host': '127.0.0.1',
-    'port': '5432',
-}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.sqlite3'
+app.config['SECRET_KEY'] = "random string"
 
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1132@127.0.0.1:5432/threebody'
 
 db = SQLAlchemy(app)
 
-class tasks(db.Model):
-   id = db.Column( db.Integer, primary_key = True)
-   taskname = db.Column(db.String(100))
-   created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-
-class inputs(db.Model):
+class tasks(db.Model): 
    id = db.Column(db.Integer, primary_key = True)
-   taskid = db.Column(db.Integer, db.ForeignKey('tasks.id'))
-   param1 = db.Column(db.Float(20))
-   param2 = db.Column(db.Float(20))
+   name = db.Column(db.String(100))
+   created = db.Column(db.DateTime, default=datetime.utcnow)
+   status = db.Column(db.Integer,default = 0)
+   active = db.Column(db.Integer,default = 0)
 
-class result(db.Model):
+def __init__(self,name, created,status,active):
+   self.name = name
+   self.created = created
+   self.status = status
+   self.active = active
+  
+class io(db.Model):
    id = db.Column(db.Integer, primary_key = True)
-   taskid = db.Column(db.Float, db.ForeignKey('tasks.id'))
-   result1 = db.Column(db.Float(20))
-   result2 = db.Column(db.Float(20))
+   task_id = db.Column(db.Integer)
+   category = db.Column(db.Integer,default = 0)
+   m1 = db.Column(db.Float(50))
+   m2 = db.Column(db.Float(50))
+   m3 = db.Column(db.Float(50))
+   x1 = db.Column(db.Float(50))
+   y1 = db.Column(db.Float(50))
+   vx1 = db.Column(db.Float(50))
+   vy1 = db.Column(db.Float(50))
+   x2 = db.Column(db.Float(50))
+   y2 = db.Column(db.Float(50))
+   vx2 = db.Column(db.Float(50))
+   vy2 = db.Column(db.Float(50))
+   x3 = db.Column(db.Float(50))
+   y3 = db.Column(db.Float(50))
+   vx3 = db.Column(db.Float(50))
+   vy3 = db.Column(db.Float(50))
 
 
+def __init__(self, task_id, category):
+   self.task_id = task_id
+   self.category = category
+   self.m1 = m1
+   self.m2 = m2
+   self.m3 = m3
+   self.x1 = x1
+   self.y1 = y1
+   self.vx1 = vx1
+   self.vy1 = vy1
+   self.x2 = x2
+   self.y2 = y2
+   self.vx2 = vx2
+   self.vy2 = vy2
+   self.x3 = x3
+   self.y3 = y3
+   self.vx3 = vx3
+   self.vy3 = vy3
+   
 @app.route('/')
 def show_all():
-   return render_template('show_all.html', tasks = tasks.query.all(), inputs = inputs.query.all() )
+   return render_template('show_all.html', tasks = tasks.query.all(), io = io.query.all())
 
 @app.route('/new', methods = ['GET', 'POST'])
 def new():
    if request.method == 'POST':
-      if not request.form['taskname'] or not request.form['id']:
+      if not request.form['name']:
          flash('Please enter all the fields', 'error')
       else:
-         task = tasks(request.form['id'],request.form['taskname'],request.form['created'])
-         input = inputs(request.form['id'],request.form['taskid'],request.form['param1'],request.form['param2'])
-         
+         task = tasks(name = request.form['name'])
          db.session.add(task)
-         db.session.add(input)
          db.session.commit()
-         
-         flash('Task was successfully added')
+         ios = io(m1 = request.form['m1'],m2 = request.form['m2'],m3 = request.form['m3'],x1 = request.form['x1'],y1 = request.form['y1'],vx1 = request.form['vx1'],vy1 = request.form['vy1'],x2 = request.form['x2'],y2 = request.form['y2'],vx2 = request.form['vx2'],vy2 = request.form['vy2'],x3 = request.form['x3'],y3 = request.form['y3'],vx3 = request.form['vx3'],vy3 = request.form['vy3']  )
+         db.session.add(ios)
+         db.session.commit()
+
+         flash('Record was successfully added')
          return redirect(url_for('show_all'))
    return render_template('new.html')
 
