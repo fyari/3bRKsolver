1bb754b34e60d29b6c73afb30c89c13eea6c77be
fyari <fyari@users.noreply.github.com>
2020-01-27 01:37:57 +0100

    Update app.py

--- master/app.py
+++ master/app.py
@@ -1,78 +1,162 @@
-#!flask/bin/python
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+#from flask_sqlalchemy import SQLAlchemy
+#import datetime
 
-rk = []
-# rk = (rk_id,task_id)
-# rk [(1,20),(2,21)]
-
-#db
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 
 app = Flask(__name__)
-
-db = [("")]
-
-tasks = [
-    {
-        'id': 1,
-        'name': u'my task 1',
-        'created': u'10102020', 
-        'status': 'result_ready'
-    },
-    {
-        'id': 2,
-        'name': u'my task 2',
-        'created': u'10102020', 
-        'status': 'result_not_ready'
-    },
-    {
-        'id': 3,
-        'name': u'my task 3',
-        'created': u'10102020', 
-        'status': 'not_computed'
-    },
-    {
-        'id': 4,
-        'name': u'my task 4',
-        'created': u'10102020', 
-        'status': 'not_computed'
-    }
-]
-
-# models : tasks , result , inputs
-
-def get_all_tasks():
-    return []
+api = Api(app)
 
 
-def get_not_computed_tasks():
-    return [1]
+#parser = reqparse.RequestParser()
+#parser.add_argument('task')
+
+#app.config['DEBUG'] = True
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1132@127.0.0.1:5432/threebody'
+
+engine = create_engine('postgresql://postgres:postgres@localhost/threebody')
+try:
+    con = engine.connect()
+        
+except Exception:
+    print("FATAL ERROR IN MASTER NODE !!! ")
+
+RKSOLVERS = {}
+
+def registerSolver():
+    id = 0
+    if len(RKSOLVERS) == 0:
+        RKSOLVERS[1] = '0'
+        id = 1
+    else:  
+        id = RKSOLVERS.keys()[-1] + 1
+        RKSOLVERS[id] = 0
+    return  {'id' : id }
+
+def setSolverTask(rk_id,taskid):
+    RKSOLVERS[rk_id] = taskid
+def getSolverTask(rk_id):
+    return RKSOLVERS[rk_id] 
+def checkTaskAssigned(taskid):
+    pass
 
 
-@app.route('/master/save_result/<float:x>/<float:y>', methods=['GET'])
-def save_result(x,y):
-    # read result from incoming json
-    # save result in database
-    # update rk if needed
-    return jsonify({'savedx': x,'savedy':y})
+
+def getTaskID(taskname):
+    d = {}
+    d['taskname'] = taskname
+    rs = con.execute(text("""select * from task where taskname = :taskname """), **d)
+    rss = rs.fetchone()
+    id = rss[0]
+    return id
+
+def setTaskStatus(taskid , status):
+    d = {}
+    d['id'] = int(taskid)
+    d['sts'] = int(status)
+    #print d
+    #trans = con.begin()
+    try:
+        rs = con.execute(text("""update task set status = :sts where id = :id """), **d)
+        return {'status':'OK'}
+        #trans.commit()
+    except:
+        #trans.rollback()
+        #raise
+        return {'status':'ERROR'}
 
 
-@app.route('/master/get_tasks/<int:rk_id>', methods=['GET'])
-def get_tasks(rk_id):
-    task = get_not_computed_tasks()
-    if len(task) == 0:
-	return jsonify({'task':1})
-    elif len(task) == 1:
-	return jsonify(tasks[1])
+def saveSymbolResult(taskid , symbol , value):
+    d = {}
+    d['id'] = taskid
+    d['s'] = symbol
+    d['v'] = float(value)
+    #d['id'] = getTaskID(con,taskname)
+    #trans = con.begin()
+    try:
+        rs = con.execute(text("""insert into io (task_id , type , symbol , value ) values (:id , 1 , :s , :v) """), **d)
+        #trans.commit()
+        return {'status':'OK'}
+    except:
+        #trans.rollback()
+        #raise
+        return {'status':'ERROR'}
+
+def getTaskInputs(taskname):
+    d = {}
+    d['taskname'] = taskname
+    d['id'] = getTaskID(taskname)
+    #print d['id']
+    rs = con.execute(text("""select symbol , value from io where task_id = :id and type = 0; """), **d)
+    rss = rs.fetchall()
+    # error NoneTpe object should be solved
+    if rss == None:
+        return {'error':'NOINPUTS'}
+    try:
+        for i in rss:
+            d[i[0]] = i[1]
+    except:
+        return {'error':'NOINPUTS'}
+    return d
+
+
+
+def getNewTask():
+    task = []
+    # task should be activate by webapp after inserting all inputs
+    rs = con.execute("select * from task where status = 0 and active = 1 ")
+    # error NoneTpe object should be solved
+    if (rs):
+        job = rs.fetchone() 
+        try:    
+            for iter in job:
+                task.append(iter)
+        except:
+            return {'id':-1}
+ 
     else:
-    #decide which task to send---
-    #update rk.append() or rk.update()
-    # send jesonify task to rk 
-	return jsonify({'tasks': tasks})
+        #print task
+        return {'id':-1}
+    #print task
+    return {'id':task[0] , 'taskname':task[1] }
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
 
+
+class RKSolver(Resource):
+    def get(self):
+        return registerSolver()
+
+class Task(Resource):
+    def get(self,rk_id):
+        # get new task , get inputs , (set task status = 1 , return json to rk )
+        d = getNewTask()
+        if d['id'] == -1 :
+            return {'status':'NOTASK'}
+        s = getTaskInputs(d['taskname'])
+        setTaskStatus(d['id'],1)
+        return s
+
+        
+class Result(Resource):
+    def get(self,taskid , symbol , value):
+        d = saveSymbolResult(taskid,symbol,value)
+        return d
+
+class Status(Resource):
+    def get(self,taskid, status):
+        d = setTaskStatus(taskid , status)
+        return d
+
+api.add_resource(RKSolver, '/registerrk')
+#api.add_resource(Task, '/task')
+api.add_resource(Task, '/task/<rk_id>')
+#api.add_resource(Result, '/save/<taskname>/<symbol>/<value>')
+api.add_resource(Result, '/save/<taskid>/<symbol>/<value>')
+api.add_resource(Status, '/status/<taskid>/<status>')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
+ 
